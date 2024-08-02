d141dd76180e72b8ae3661259cd2f5afaa37d793
mastachii <mastachii273@gmail.com>
2024-08-02 20:42:26 +0800

    Started with calculator functionality

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const numberButtons = document.getElementById('numbers')
     , operatorButtons = document.getElementById('buttons-right')
     , extraButtons = document.getElementById('extra-buttons')
-    , test = document.getElementById('text');
+    , displayInput = document.getElementById('display');
 
 
 numberButtons.addEventListener('mousedown', changeColorDown);
@@ -23,7 +23,7 @@ function changeColorDown (e){
 
         case 'numbers':
             e.target.style.backgroundColor = '#fbfaf9';
-            test.append(e.target.textContent)
+            // test.append(e.target.textContent)
             break;
 
         case 'buttons-right':
@@ -56,8 +56,55 @@ function changeColorUp (e){
     }
 }
 
+// CALCULATOR OBJ
+
+let Calculator = {
+
+    values:{
+        
+        leftOperand: '',
+        rightOperand: '',
+        result: 0
+    },
+
+    operators:{
+        
+        add: 0,
+        subtract: 0,
+        multiply: 0,
+        divide: 0,
+    }
+}
 
 
+numberButtons.addEventListener('mousedown', inputNum);
+operatorButtons.addEventListener('mousedown', inputNum);
+extraButtons.addEventListener('mousedown', inputNum)
 
+// DETERMINES IF LEFT OR RIGHT HAND OPERAND GETS CHANGED
 
+let mode = 0
 
+function inputNum (e){
+
+    let currentNum;
+
+    if (String(+e.target.textContent) !== 'NaN'){ currentNum = e.target.textContent; }
+
+    if (String(+e.target.textContent) === 'NaN' && Calculator.values.leftOperand !== '') mode = 1;
+
+    if(currentNum === undefined) return
+
+    switch (mode){
+
+        case 0:
+            Calculator.values.leftOperand += currentNum;
+            break;
+
+        case 1: 
+            Calculator.values.rightOperand += currentNum
+            break;
+    }
+
+    
+}
